7d5f7854465e4f461d5be7cf06c7211673221c16
yutadd <spyk4m49@gmail.com>
2022-12-15 22:46:27 +0900

    add optimize argment to make option for compare.

--- lv2_loop/loop.s
+++ lv2_loop/loop.s
@@ -1,27 +1,23 @@
 .text
 .globl main
 main:
-    pushq	%rbp
-	movq	%rsp, %rbp
-	subq	$16, %rsp
-	movl	$0, -4(%rbp)
-	jmp	.L2
-.L3:
-    leaq	message(%rip), %rax
-	movq	%rax, %rdi
-	call	puts@PLT
-	addl	$1, -4(%rbp)
-.L2:
-	cmpl	$9, -4(%rbp)
-	jle	.L3
+    push	%rbp
+	mov	%rsp, %rbp # movlのように、サイズを指定していないため、コンパイル時にワーニングが出る。
+	sub	$16, %rsp
+	mov	$0, -4(%rbp)
+	jmp	L2
+L3:
+    /*leaq	message(%rip), %rax
+	mov	%rax, %rdi
+	call	puts@PLT*/
+	add	$1, -4(%rbp)
+    # ここで呼び出し元に戻ったりせず、そのまま下の行が実行される。
+L2:
+	cmp	$50000000, -4(%rbp)
+	jle	L3
         # exit(0)
     mov     $0x3c, %rax               # set operation code 0x3c
     xor     %rdi, %rdi              # set 0 to rdi
     syscall                         # execute exit
 .data
 message:    .asciz "hello"
-
-/*	leaq	message(%rip), %rax
-	movq	%rax, %rdi
-	call	puts@PLT
-    */
